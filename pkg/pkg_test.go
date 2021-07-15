59fd3bb5412c657c015553ad71816aabbf139c2a
Iskandarov Lev Andreevich <liskandarov@ozon.ru>
2021-07-16 00:09:32 +0300

    minimized

--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -1,15 +1,61 @@
 package pkg_test
 
 import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"sync"
 	"testing"
-
-	"github.com/leviska/go-unsafe-gc/other"
-	"github.com/leviska/go-unsafe-gc/pkg"
+	"unsafe"
 )
+
+func StringToByteUnsafe(s string) []byte {
+	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	var sh reflect.SliceHeader
+	sh.Data = strh.Data
+	sh.Len = strh.Len
+	sh.Cap = strh.Len
+	return *(*[]byte)(unsafe.Pointer(&sh))
+}
+
+func Convert(s []byte) []byte {
+	return StringToByteUnsafe(string(s))
+}
+
+type T struct {
+	S []byte
+}
+
+func Copy(s []byte) T {
+	return T{S: s}
+}
+
+func Mid(a []byte, b []byte) []byte {
+	fmt.Printf("%p %p\n", a, b)
+	sum := 0
+	for i := 0; i < 10000; i++ {
+		c := make([]byte, 1024*1024)
+		sum += len(c)
+	}
+	fmt.Println(sum)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		runtime.GC()
+		wg.Done()
+	}()
+	go func() {
+		b = b[1:2]
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Printf("%p %p\n", a, b)
+	return b
+}
 
 func TestSomething(t *testing.T) {
 	str := "123"
-	a := pkg.Convert([]byte(str))
-	b := other.Copy(a)
-	pkg.Mid(a, b.S)
+	a := Convert([]byte(str))
+	b := Copy(a)
+	Mid(a, b.S)
 }
